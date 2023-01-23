c684ee20cd690cbe2351e72685e7cf1edd728e48
Chasti <chastytim@mail.ru>
2023-01-23 10:16:35 +0300

    complete second view

--- api/source/db_funcs.py
+++ api/source/db_funcs.py
@@ -1,5 +1,5 @@
 from db.connection import Session
-from db.models import City
+from db.models import City, Data
 from loguru import logger
 
 
@@ -23,3 +23,54 @@ def add_city_to_db(name, ow_id):
     session.add(city)
     session.commit()
     session.close()
+
+
+def get_unsorted_data():
+    session = Session()
+    data = []
+    for i in session.query(City).all():
+        city = i.__dict__
+        obj = session.query(Data).filter_by(city_id=city['id']).order_by(Data.id.desc()).first()
+        obj = obj.__dict__
+        logger.info(obj)
+        data.append({
+            'city': city['name'],
+            'data':{
+                'temperature': obj['temperature'],
+                'wind_speed': obj['wind_speed'],
+                'pressure': obj['pressure']
+            }
+        })
+    return data
+
+
+def get_sorted_data(name):
+    name = name.capitalize()
+    session = Session()
+    data = []
+    cities = [i.__dict__ for i in session.query(City).all()]
+    matched = []
+    for index, item in list(enumerate(cities)):
+        if item['name'] == name:
+            matched.append(item)
+            cities.remove(item)
+            break
+    for j in range(len(name)):
+        for index, item in list(enumerate(cities)):
+            if item['name'] in name[:len(name)-j] or name[:len(name)-j] in item['name']:
+                matched.append(item)
+                cities.remove(item)
+    matched.extend(cities)
+
+    for i in matched:
+        obj = session.query(Data).filter_by(city_id=i['id']).order_by(Data.id.desc()).first()
+        obj = obj.__dict__
+        data.append({
+            'city': i['name'],
+            'data':{
+                'temperature': obj['temperature'],
+                'wind_speed': obj['wind_speed'],
+                'pressure': obj['pressure']
+            }
+        })
+    return data
--- api/views/statistic.py
+++ api/views/statistic.py
@@ -1,5 +1,17 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from loguru import logger
+
+from source.db_funcs import get_unsorted_data, get_sorted_data
 
 
 stat_router = APIRouter()
+
+
+@stat_router.get('/last_weather', response_class=JSONResponse)
+async def get_last_weather(search: str=''):
+    if len(search) > 0:
+        if search[0] == '{':
+            search = search[1:-1]
+        return get_sorted_data(search)
+    return get_unsorted_data()
